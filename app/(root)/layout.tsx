d6a93ca24d21162091d15dd5442028c092a8bbff
Yavuz Yalçıntaş <myavuzyalcintas@gmail.com>
2024-06-16 20:18:40 +0300

    temp fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -46,7 +46,7 @@ export default async function RootLayout({
               <div className="w-full max-w-4xl">{children}</div>
             </section>
             {/* @ts-ignore */}
-            <RightSidebar />
+            {/* <RightSidebar /> */}
           </main>
           <Toaster />
 
